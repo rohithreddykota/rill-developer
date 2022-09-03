6e352b57be0d8ed0a20d8440bf4924247a9788ae
Mike Godwin <mike.godwin@rilldata.com>
2022-09-03 13:00:09 -0700

    maybe a vimeo issue?

--- docs/src/components/Vimeo.js
+++ docs/src/components/Vimeo.js
@@ -3,24 +3,7 @@ import React from 'react';
 export default function Vimeo({vimeoId}) {
   let vimeoUrl = 'https://player.vimeo.com/video/' + vimeoId + '?muted=1&autopause=0&autoplay=1&loop=1&title=0&byline=0&portrait=0';
   return (
-    <div style={{
-      padding: '56.23% 0 0 0', 
-      position: 'relative'
-    }}>
-      <iframe 
-        src={vimeoUrl}
-        style={{
-          position: 'absolute', 
-          top: 0, 
-          left: 0, 
-          width: '100%', 
-          height: '100%'
-        }}
-        frameBorder='0'
-        autoPlay
-        allowFullScreen>
-      </iframe>
-    </div>
+    <div>spoop</div>
   );
 }
 
