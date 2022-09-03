e82bd59f3d4016fe395ae1e53a792a963819df06
Mike Godwin <mike.godwin@rilldata.com>
2022-09-03 13:05:19 -0700

    different test page

--- docs/docs/test-page.md
+++ docs/docs/test-page.md
@@ -4,6 +4,5 @@ title: test page
 
 ![Kapture 2022-07-21 at 15 34 45](https://user-images.githubusercontent.com/5587788/180313797-ef50ec6e-fc2d-4072-bb77-b2acf59205d7.gif "732257485")
 
-[CLI](/cli)
 
 [Rill Terms of Service](https://rilldata.com/legal/tos) and [Privacy Policy](https://rilldata.com/legal/privacy).
--- docs/src/components/Vimeo.js
+++ docs/src/components/Vimeo.js
@@ -1,9 +1,26 @@
 import React from 'react';
 
-export default function Vimeo({vimeoId}) {
-  let vimeoUrl = 'https://player.vimeo.com/video/' + vimeoId + '?muted=1&autopause=0&autoplay=1&loop=1&title=0&byline=0&portrait=0';
+export default function Vimeo(props) {
+  let vimeoUrl = 'https://player.vimeo.com/video/' + props.vimeoId + '?muted=1&autopause=0&autoplay=1&loop=1&title=0&byline=0&portrait=0';
   return (
-    <div>spoop</div>
+    <div style={{
+      padding: '56.23% 0 0 0', 
+      position: 'relative'
+    }}>
+      <iframe 
+        src={vimeoUrl}
+        style={{
+          position: 'absolute', 
+          top: 0, 
+          left: 0, 
+          width: '100%', 
+          height: '100%'
+        }}
+        frameBorder='0'
+        autoPlay
+        allowFullScreen>
+      </iframe>
+    </div>
   );
 }
 
