73130de32871f67b66b7415c5795c6c52fcb065a
Mike Godwin <mike.godwin@rilldata.com>
2022-09-03 12:34:59 -0700

    test-page attempt

---
+++ docs/docs/test-page.md
@@ -0,0 +1,10 @@
+---
+title: test page
+---
+
+
+[CLI](/cli)
+
+[Rill Terms of Service](https://rilldata.com/legal/tos)
+
+[Privacy Policy](https://rilldata.com/legal/privacy)
--- docs/src/theme/MDXComponents/A.js
+++ docs/src/theme/MDXComponents/A.js
@@ -3,7 +3,7 @@ import Link from '@docusaurus/Link';
 import Video from '@site/src/components/Video';
 
 export default function MDXA(props) {
-  if ((props.href.endsWith("gif") || props.href.endsWith("mp4")) && (typeof props.title !== 'undefined')) {
+  if ((props.href.endsWith(".gif") || props.href.endsWith(".mp4")) && (typeof props.title !== 'undefined')) {
     return (
       <Video vimeoId={props.title} />
     )
